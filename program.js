403189b06be67381b501e40bcdd6a3ba3a5acd2a
app182019 <47757230+app182019@users.noreply.github.com>
2019-02-21 20:19:52 +0200

    Update program.js

--- program.js
+++ program.js
@@ -1,10 +1,17 @@
-// 1
+// read numbers first way (less favorite)
 //var x1 = prompt("Please enter first number", 0)
 //var y1 = prompt("Please enter second number", 0)
 //var z1 = parseInt(x) + parseInt(y)
 
-// 2
+// 1
 var x = parseInt( prompt("Please enter first number", 0) )
 var y = parseInt( prompt("Please enter second number", 0) )
-var z = x + y
-console.log(z)
+if (x > y)
+  {
+    // if true
+    alert(x)
+  }
+else
+  {
+    alert(y)
+  }
