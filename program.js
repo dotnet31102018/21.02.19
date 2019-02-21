a467b72e51bafe21d175c6afbacb2106145cad06
app182019 <47757230+app182019@users.noreply.github.com>
2019-02-21 20:14:51 +0200

    Create program.js

---
+++ program.js
@@ -0,0 +1,10 @@
+// 1
+//var x1 = prompt("Please enter first number", 0)
+//var y1 = prompt("Please enter second number", 0)
+//var z1 = parseInt(x) + parseInt(y)
+
+// 2
+var x = parseInt( prompt("Please enter first number", 0) )
+var y = parseInt( prompt("Please enter second number", 0) )
+var z = x + y
+console.log(z)
